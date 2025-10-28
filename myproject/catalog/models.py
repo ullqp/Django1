788a78ba9268cb9cb03c6d623f927713fcfba8b9
ullqp <waynaungelneal@gmail.com>
2025-10-28 20:44:51 +0300

    Add files via upload

--- myproject/catalog/models.py
+++ myproject/catalog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.name} {self.description}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to='images/', verbose_name='Изображение')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='Категория')
+    price = models.FloatField(verbose_name='Цена')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now_add=True, verbose_name='Время изменения')
+
+
+    def __str__(self):
+        return f'{self.name} {self.description}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
--- myproject/config/settings.py
+++ myproject/config/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -73,10 +75,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+load_dotenv()
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':  os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT', default='5432'),
     }
 }
 
